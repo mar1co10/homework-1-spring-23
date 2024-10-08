89afbeaa8f5515b3d4d5846a40df63e5119fc20b
mar1co10 <arevalom@purdue.edu>
2024-10-08 19:36:54 -0400

    Bank.sol

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -11,13 +11,28 @@ contract Bank {
 
     function createAccount () public {
         // TODO : Implement the logic to create an account
+        require(accounts[msg.sender].owner != address(0), "Account does not exist");
+
+        accounts[msg.sender] = Account({
+        owner: msg.sender,
+        balance: 0
     }
 
     function deposit ( uint256 amount ) public {
         // TODO : Implement the logic to deposit an amount
+        require(accounts[msg.sender].owner != address(0), "Account does not exist");
+
+        accounts[msg.sender].balance += amount;
+
+        
     }
 
     function withdraw ( uint256 amount ) public {
         // TODO : Implement the logic to withdraw an amount
+        require(accounts[msg.sender].owner != address(0), "Account does not exist");
+
+        require(accounts[msg.sender].balance >= amount, "Insufficient balance");
+
+        accounts[msg.sender].balance -= amount;
     }
 }
